6588d110dc53a29cde1b6907f02e49750b3c0366
ThatJuanGuy <ThatJuanGuy@users.noreply.github.com>
2021-04-29 19:48:53 -0500

    File data is now saved for single upload button

--- src/components/Left/UploadButton/UploadButton.js
+++ src/components/Left/UploadButton/UploadButton.js
@@ -8,30 +8,28 @@ export default class UploadButton extends Component {
   state = {fileName: null,
            fileContents: null
           };
-
+  
   handleInput = event => {
     if(event.target.files[0] !== undefined ) {
+      // read name of the file
       this.setState({fileName: event.target.files[0].name});
+      // read the file contents
+      var fileReader = new FileReader();
+      fileReader.onloadend = () => {this.setState({fileContents: fileReader.result});}
+      fileReader.readAsText(event.target.files[0]);
+      // reset the state of the file reader
+      // This is to avoid a bug where it wouldn't register if you 
+      // uploaded the same file twice after reomoving it
+      event.target.value = "";
     }
-    else {
-      console.log("YEET");
-    }
-    
   }
+
+  
 
   handleRemove = () => {
-    this.setState({fileName: null});
-    /* Haven't tested because generate doesn't exist yet but
-       this could be hiding the file input from the user but 
-       leaving it existing. So if the user hits generate after
-       think they removed it, it will still generate as if the
-       previous file was still displayed
-
-       Adding the same file after removing it will cause an error
-       where the file will not appear as added. Switching to onInput 
-       did not fix this issue.
-    */
-  }
+    this.setState({fileName: null,
+                   fileContents:null});
+    }
 
   render() {
     return (
