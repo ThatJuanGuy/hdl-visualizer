c8baf37d385cc6f273e07a80ac48254301fabf0d
ThatJuanGuy <ThatJuanGuy@users.noreply.github.com>
2021-05-07 12:16:06 -0500

    Added flask to app

---
+++ flask/app.py
@@ -0,0 +1,25 @@
+#imports from visualize so this works because they aren't brought in for some reason
+from nand2tetris_hdl_parser import parse_hdl
+import re
+import io
+from topsort import Chip_Graph, Chip_Graph_BFS
+from schemdraw import logic
+from schemdraw import elements
+from schemdraw import Drawing, ImageFormat
+from schemdraw.parsing import logicparse
+from schemdraw.elements import ElementDrawing
+#new imports
+from visualize import drawHDL #functions
+from flask import Flask, Response
+import os
+
+app = Flask(__name__)
+
+@app.route('/draw/<fileContent>.svg', methods=['GET'])
+def generateDrawing(fileContent):
+    
+    figBytes = drawHDL(fileContent)
+    return Response(figBytes, mimetype='image/svg+xml')
+
+if __name__ == '__main__':
+    app.run()
