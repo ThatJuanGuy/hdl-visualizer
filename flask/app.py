a937481a96e1a247ce53d25dcac172c4ae6bcd4d
ThatJuanGuy <ThatJuanGuy@users.noreply.github.com>
2021-05-07 20:04:42 -0500

    app.py changes
    Hopefully this'll do stuff that makes it work

--- flask/app.py
+++ flask/app.py
@@ -25,4 +25,4 @@ def generateDrawing(fileContent):
     return Response(figBytes, mimetype='image/svg+xml')
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=False, port=os.environ.get('PORT', 80))
+    app.run(debug=False)
